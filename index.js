8ca01af4a06f81a8254f5e837498e8858e480995
fignaomi <naomi.figueiredo@gmail.com>
2021-05-19 23:35:37 -0300

    Aula 3

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const configExpress = require('./config/configExpress');
+const conexao = require('./infra/conexao');
+const Tabelas = require('./infra/Tabelas');
 
 
-/*let em versoes mais antigas*/ app = configExpress()
+conexao.connect(error => {
+    if (error) {
+        throw error;
+    };
 
-app.listen(3000, () =>console.log('Servidor rodando na porta 3000'));
+    Tabelas.init(conexao);
 
+    app = configExpress();
+
+    app.listen(3000, () =>{
+        console.log('Servidor rodando na porta 3000')
+    });
+
+});
--- infra/conexao.js
+++ infra/conexao.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const conexao = mysql.createConnection({
     host: 'localhost',
-    port: 3360,
+    port: 3306,
     user: 'root',
     password: '1234',
     database: 'agendawcc'
